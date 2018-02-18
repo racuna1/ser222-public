8617fd831cf13baf3e05d504d0287d17147290b7
Ruben Acuna <racuna1@asu.edu>
2018-02-17 23:34:21 -0700

    Unit 02_02: added source code samples.

---
+++ 02_02_mergesort/TopdownMergesort.java
@@ -0,0 +1,78 @@
+/**
+ * A sample insertion sort implementation.
+ * 
+ * @author Sedgewick and Wayne, Acuna
+ * @version 1.0
+ */
+
+public class TopdownMergesort {
+    
+    public static void sort(Comparable[] a) {
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length-1);
+        
+        assert isSorted(a);
+    }
+    
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+    
+    public static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi)
+    {
+        assert isSorted(a, lo, mid);
+        assert isSorted(a, mid+1, hi);
+        
+        int i = lo, j = mid+1;
+        
+        for(int k = lo; k <= hi; k++)
+            aux[k]=a[k];
+        
+        // merge back to a[]
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)              a[k] = aux[j++];
+            else if (j > hi)               a[k] = aux[i++];
+            else if (less(aux[j], aux[i])) a[k] = aux[j++];
+            else                           a[k] = aux[i++];
+        }
+        
+        assert isSorted(a, lo, hi);
+    }
+    
+    //helper
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+    
+    private static void show(Comparable[] a) {
+        for(int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
+    }
+    
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1]))
+                return false;
+        
+        return true;
+    }
+    
+    private static boolean isSorted(Comparable[] a, int lo, int hi) {
+        for (int i = lo + 1; i <= hi; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        String[] a = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
+        sort(a);
+        assert isSorted(a);
+        show(a);
+    }
+}
